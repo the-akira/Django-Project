50674708c182752fda33eb0636dbf34a7af1e8e0
Gabriel Felippe <gabrielfelippe90@gmail.com>
2021-07-12 11:00:17 -0300

    Ambiente Virtual removido

--- myenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/talantyr/Documentos/Projetos/Python Experimentos/django_project/myenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
